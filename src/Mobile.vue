339ec08d52f00b3a1e25969157e690d97c9d1e56
wawa16507 <1650713339@qq.com>
2018-07-14 11:53:58 +0800

    update

--- src/Mobile.vue
+++ src/Mobile.vue
@@ -1,27 +1,20 @@
 <template>
   <div id="app">
-    <div id="content">
-      <StyleEditor ref="styleEditor" :code="currentStyle"></StyleEditor>
-      <ResumeEditor ref="resumeEditor" :markdown="currentMarkdown" :enableHtml="enableHtml"></ResumeEditor>
-    </div>
-    <div id="foot">
-      <ThankEditor ref="thankEditor" :markdown="currentThankMarkdown" :enableHtml="enableHtml"></ThankEditor>
-    </div>
+    <StyleEditor ref="styleEditor" :code="currentStyle"></StyleEditor>
+    <ResumeEditor ref="resumeEditor" :markdown="currentMarkdown" :enableHtml="enableHtml"></ResumeEditor>
   </div>
 </template>
 
 <script>
   import StyleEditor from './components/StyleEditor'
   import ResumeEditor from './components/ResumeEditor'
-  import ThankEditor from './components/ThankEditor'
   import './assets/reset.css'
 
   export default {
     name: 'app',
     components: {
       StyleEditor,
-      ResumeEditor,
-      ThankEditor
+      ResumeEditor
     },
     data() {
       return {
@@ -31,42 +24,46 @@
         fullStyle: [
           `/*
 * Inspired by http://strml.net/
-* 源码地址 https://github.com/wawa16507/wawa
+* 源码地址 https://github.com/sitexa/anires
 * 大家好，我是黄珂。
-* 我来改一个网页！
+* 我来改一个网页（一个大神的源码）。
 */
 
 /* 给所有元素加上过渡效果 */
 * {
-  transition: all .1s;
+  transition: all .2s;
 }
 /* 设置背景颜色 */
 html {
-  color: rgb(222,222,222); background: rgb(0,64,64);
+  color: rgb(222,222,222);
+  background: rgb(0,43,54);
 }
-#content{
-  height:70vh;
-  margin:0;
-}
-#foot{
-  height:29vh;
-  margin:0;
-}
-
 /* 设置边框 */
 .styleEditor {
   padding: .5em;
   border: 1px solid;
-  margin: .5em;
   overflow: auto;
-  width: 50vw; height: 70vh;
-  background: rgb(20,20,20);
+  width: 90vw;
+  margin: 2.5vh 5vw;
+  height: 90vh;
+}
+/* 太高了 */
+.styleEditor {
+  height: 45vh;
 }
 /* 代码高亮 */
-.token.selector{ color: rgb(130,150,0); }
-.token.property{ color: rgb(190,140,0); }
-.token.punctuation{ color: yellow; }
-.token.function{ color: rgb(40,160,150); }
+.token.selector{
+  color: rgb(133,153,0);
+}
+.token.property{
+  color: rgb(187,137,0);
+}
+.token.punctuation{
+  color: yellow;
+}
+.token.function{
+  color: rgb(42,161,152);
+}
 
 /* 加3D效果 */
 html{
@@ -74,30 +71,28 @@ html{
 }
 .styleEditor {
   position: fixed; left: 0; top: 0;
-  -webkit-transition: none;
-  transition: none;
-  -webkit-transform: rotateY(10deg) translateZ(-100px) ;
-          transform: rotateY(10deg) translateZ(-100px) ;
+  transform: rotateX(-10deg) translateZ(-50px) ;
 }
 
 /* 准备一个编辑器 */
 .resumeEditor{
-  position: fixed; right: 0; top: 0;
-  padding: .5em;  margin: .5em;
-  width: 50vw; height: 70vh;
+  position: fixed;
+  top: 50%; left: 0;
+  padding: .5em;  margin: 2.5vh;
+  width: 95vw; height: 45vh;
   border: 1px solid;
-  background: rgb(200,200,200); color: #222;
+  background: white; color: #222;
   overflow: auto;
-  -webkit-transition: none;
-  transition: none;
-  -webkit-transform: rotateY(-10deg) translateZ(-100px) ;
-          transform: rotateY(-10deg) translateZ(-100px) ;
 }
 /* 开始吹牛逼 */
-`, `
+
+
+`,
+          `
 /*将Markdown格式翻译成HTML
  *再对HTML加点样式
 */
+`, `
 .resumeEditor{
   padding: 2em;
 }
@@ -126,68 +121,10 @@ html{
   padding: .5em;
   background: #ddd;
 }
-`, `/* 这是一个大神的网站。
- * 希望我们以后都能有一个自己的这样的网站。
- */
-.styleEditor{
-    width:50vw;height:70vh;
-}
-
-.resumeEditor{
-   width:50vw;height:70vh;
-}
-
-.thankEditor{
-  position: relative; left: 0; top: 0;
-  background: #E9D9BB;
-  color: #001C42;
-  overflow: auto;
-}
-
-.thankEditor {
-  width: 99vw; height: 45vh;
-  border: 1px solid #ccc;
-  font-size: .9em;
-}
-`,`
-.thankEditor{
-  padding: .5em;  margin: .5em; margin-top:1em;
-}
-
-.thankEditor ul,.thankEditor ol{
-  list-style: none;
-}
-.thankEditor ul> li::before{
-  content: '☞'; color: red;
-  margin-right: .5em;
-}
-.thankEditor ol {
-  counter-reset: section;
-}
-.thankEditor ol li::before {
-  counter-increment: section;
-  content: counters(section, "☞") " ";
-  margin-right: .5em;
-}
-
-.thankEditor{
-  width: 99vw; height: 45vh;
-  -webkit-transition: none;
-  transition: none;
-  -webkit-transform: rotateX(-10deg) translateZ(-200px);
-          transform: rotateX(-10deg) translateZ(-200px);
-}
-
 `],
         currentMarkdown: '',
-        currentThankMarkdown: '',
         fullMarkdown: `黄珂
 ====
-
-坐标：湖南长沙。
-
-资深卵件工程师，资深吹逼大师
-
 技能
 ====
 
@@ -263,9 +200,7 @@ html{
 * 我还会爬妹子图，爬各种资源。本来想组一个团队把资源打包，上传到百度网盘，然后靠招收代理，卖百度网盘创业发家致富的，但是不知道为什么没人肯加入我。
 * 这一年其实挺快的不是吗。离开了一些人，但也认识了一些人，把那一群人的吹逼技巧教给了这一群人，那边认识了太久的那种腻的感觉升华成了淡淡的思念，这边初识的惊艳与窃喜也远去变成了如今的嫌弃与默契。我们得慢下来。
 * 有什么好玩的都可以来滴滴我交流学习。
-
-
-  `
+`
       }
     },
     created() {
@@ -274,17 +209,18 @@ html{
 
     methods: {
       makeResume: async function () {
-        await this.progressivelyShowStyle(0);
-        await this.progressivelyShowResume();
-        await this.progressivelyShowStyle(1);
-        await this.showHtml();
-        await this.progressivelyShowStyle(2);
-        await this.progressivelyShowThanks();
-        await this.progressivelyShowStyle(3)
+        await this.progressivelyShowStyle(0)
+        await this.progressivelyShowResume()
+        await this.progressivelyShowStyle(1)
+        await this.showHtml()
+        await this.progressivelyShowStyle(2)
       },
-      showHtml() {
+      showHtml: function () {
         return new Promise((resolve, reject) => {
           this.enableHtml = true
+          this.$nextTick(() => {
+            this.$refs.resumeEditor.goTop()
+          })
           resolve()
         })
       },
@@ -293,9 +229,7 @@ html{
           let interval = this.interval
           let showStyle = (async function () {
             let style = this.fullStyle[n]
-            if (!style) {
-              return
-            }
+            if (!style) { return }
             // 计算前 n 个 style 的字符总数
             let length = this.fullStyle.filter((_, index) => index <= n).map((item) => item.length).reduce((p, c) => p + c, 0)
             let prefixLength = length - style.length
@@ -335,26 +269,6 @@ html{
           }
           showResume()
         })
-      },
-      progressivelyShowThanks() {
-        return new Promise((resolve, reject) => {
-          let length = this.thanksMarkdown.length
-          let interval = this.interval
-          let showThanks = () => {
-            if (this.currentThankMarkdown.length < length) {
-              this.currentThankMarkdown = this.thanksMarkdown.substring(0, this.currentThankMarkdown.length + 1)
-              let lastChar = this.currentThankMarkdown[this.currentThankMarkdown.length - 1]
-              let prevChar = this.currentThankMarkdown[this.currentThankMarkdown.length - 2]
-              if (prevChar === '\n' && this.$refs.thankEditor) {
-                this.$nextTick(() => this.$refs.thankEditor.goBottom())
-              }
-              setTimeout(showThanks, interval)
-            } else {
-              resolve()
-            }
-          }
-          showThanks()
-        })
       }
     }
   }
@@ -366,13 +280,14 @@ html{
     font-family: 'Avenir', Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
+    min-height: 100vh; position: relative;
   }
 
   html {
     min-height: 100vh;
   }
-
-  * {
+  *{
     box-sizing: border-box;
   }
+
 </style>
